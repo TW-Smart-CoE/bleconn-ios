f1bad5d44062c241e5f91c112a7d1dcacdfc3c9b
Jie Meng <jmengxy@gmail.com>
2025-04-22 17:34:27 +0800

    First commit

---
+++ BleConnSample/BleConnSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleConnSampleApp.swift
+//  BleConnSample
+//
+//  Created by Jie Meng on 2025/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BleConnSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
