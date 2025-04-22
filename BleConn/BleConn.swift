f44ea3d3fff2fa898ef2340a04dcb7a2873fd1c6
Jie Meng <jmengxy@gmail.com>
2025-04-22 15:18:34 +0800

    Initial Commit

---
+++ BleConn/BleConn.swift
@@ -0,0 +1,9 @@
+//
+//  BleConn.swift
+//  BleConn
+//
+//  Created by Jie Meng on 2025/4/22.
+//
+
+import Foundation
+
---
+++ BleConnTests/BleConnTests.swift
@@ -0,0 +1,17 @@
+//
+//  BleConnTests.swift
+//  BleConnTests
+//
+//  Created by Jie Meng on 2025/4/22.
+//
+
+import Testing
+@testable import BleConn
+
+struct BleConnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
